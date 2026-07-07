6f8222070fa3626129b959cad448c1ad0ec30d09
exy <teslawiktor@gmail.com>
2026-07-07 21:54:04 +0200

    feat: display animated loading indicator on splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:where_is_frog/screens/onboarding_screen.dart';
+import 'package:where_is_frog/widgets/loading.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key, required this.title});
@@ -19,25 +20,24 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void _navigateToHome() {
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const OnboardingScreen(title: "Where is Frog")),
-      );
-    });
-  }
+      Future.delayed(const Duration(seconds: 3), () {
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, a1, a2) => const OnboardingScreen(title: "Where is Frog"),
+            transitionDuration: Duration.zero,
+            reverseTransitionDuration: Duration.zero,
+          ),
+        );
+      });
+    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Ładowanie... 🐸',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+        child: CustomLoading(),
+      )
     );
   }
 }
